0726b7329a4ec655f109579054a60575f5fed93a
hypnopsy <vigneshr966@gmail.com>
2024-06-28 13:36:03 +0530

    Data Type Config

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 console.log("Hello World");
+
+
+
+var a=10;
+console.log(a);
+var a="Name";
+console.log(a);
+
+
+//let name="John";
+//console.log(name);
+//let name
+
+const pi = 3.14;
+console.log(pi);
+//const pi = 3.145
+
+
+//Nan
+
+let valueOne = 2;
+let valueTwo = "a";
+
+console.log(valueOne + valueTwo);
+
+console.log(valueOne + parseInt(valueTwo));
+
+
+const name = "John";
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+var y;
+
+
+console.log(typeof name)
+console.log('My name is ' + name + ' and I am ' + age);
